56620d13441dbdd9a9eb07dcbb5924b3ede4ba22
poulsonisme <alexanderipoulson@gmail.com>
2018-12-31 23:09:40 +0000

    Switcheroo

---
+++ FileInformationGeneral.h
@@ -0,0 +1,19 @@
+#include "Headers.h"
+
+#include <map>
+
+#pragma once
+
+class FileInformationGeneral {
+public:
+
+    std::string file_type();
+    std::string last_opened_date();
+    std::string permissions();
+    FileInformationGeneral(std::string path);
+
+private:
+
+    std::string m_path;
+
+};
